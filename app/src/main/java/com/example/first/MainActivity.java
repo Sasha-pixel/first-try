4e3fef9ce277adfd96b887c181d63734d4c33861
Sasha-Pixel <al.kanenckoff@yandex.ru>
2019-11-01 00:33:46 +0300

    1.7.6 Canvas

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(context; this));
+        setContentView( new MyView(this));
     }
 }
--- app/src/main/java/com/example/first/MyView.java
+++ app/src/main/java/com/example/first/MyView.java
@@ -2,6 +2,7 @@ package com.example.first;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -17,6 +18,25 @@ public class MyView extends View {
         //Здесь располагаются команды рисования
         ///...
         Paint paint = new Paint();
+        paint.setStrokeWidth(3);
         canvas.drawLine(0, 0, canvas.getWidth(),canvas.getHeight(),paint);
+        int y = 0;
+        while (y < canvas.getHeight()) {
+            canvas.drawLine(0, y,
+                    this.getWidth(), y, paint);
+            y += 30;
+        }
+        /*Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(300, 300, 200, paint);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 300, 200, paint);*/
+        /*Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(100, 100, 300, 300, paint);*/
     }
 }
