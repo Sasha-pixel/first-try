67f62e044f57c9d5a00e48cf55de4d4efb11d17d
0SouthBoss0 <mishumpel@mail.ru>
2019-11-01 11:40:22 +0300

    1.8 Animation Time

--- app/src/main/java/com/example/first/MyView.java
+++ app/src/main/java/com/example/first/MyView.java
@@ -14,12 +14,15 @@ public class MyView extends View {
 
     Paint paint = new Paint();
     int x = 0;
+    long lastTime = System.currentTimeMillis();
     @Override
     protected void onDraw(Canvas canvas){
         paint.setColor(Color.BLUE);
         canvas.drawCircle(x, 300, 40, paint);
         // готовим x для следущего кадра
+        long nowTime = System.currentTimeMillis();
         x += 2;
+        lastTime = nowTime;
         invalidate();
     }
 }
