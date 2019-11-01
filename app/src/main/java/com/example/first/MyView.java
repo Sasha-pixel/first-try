b3445a607b10feaa86f78f7d6f958cf28387bdbd
0SouthBoss0 <mishumpel@mail.ru>
2019-11-01 11:26:30 +0300

    1.8 Animation

--- app/src/main/java/com/example/first/MyView.java
+++ app/src/main/java/com/example/first/MyView.java
@@ -12,45 +12,14 @@ public class MyView extends View {
         super(context);
     }
 
+    Paint paint = new Paint();
+    int x = 0;
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        //Здесь располагаются команды рисования
-        ///...
-        Paint paint = new Paint();
-        paint.setColor(Color.GREEN);
-        paint.setStrokeWidth(10);
-        canvas.drawLine(0, 0, canvas.getWidth(),canvas.getHeight(),paint);
-        int y = 0;
-        while (y < canvas.getHeight()) {
-            canvas.drawLine(0, y,
-                    this.getWidth(), y, paint);
-            y += 60;
-        }
+    protected void onDraw(Canvas canvas){
         paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(10);
-        y = 0;
-        while (y < canvas.getHeight()){
-            canvas.drawLine(0, y, this.getWidth(), y, paint);
-            y += 100;
-        }
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(1000, 100, 500, 300, paint);
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(300, 300, 1000, 100, paint);
-        /*Paint paint = new Paint();
-        paint.setColor(Color.YELLOW);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(300, 300, 200, paint);
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(20);
-        canvas.drawCircle(300, 300, 200, paint);*/
-        /*Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(100, 100, 300, 300, paint);*/
+        canvas.drawCircle(x, 300, 40, paint);
+        // готовим x для следущего кадра
+        x += 2;
+        invalidate();
     }
 }
