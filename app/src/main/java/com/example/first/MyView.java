53faf3c6e46bbe430ff7d7301f4dafb78f447489
Sasha-Pixel <al.kanenckoff@yandex.ru>
2019-11-03 20:48:35 +0300

    1.8.7

--- app/src/main/java/com/example/first/MyView.java
+++ app/src/main/java/com/example/first/MyView.java
@@ -7,22 +7,38 @@ import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View {
+    int N = 10; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    Paint paint = new Paint();
 
     public MyView(Context context) {
         super(context);
+        for (int i = 0; i < N; i++)
+
+        {
+            x[i] = (float) (Math.random() * 500);
+            y[i] = (float) (Math.random() * 500);
+            vx[i] = (float) (Math.random() * 6 - 3);
+            vy[i] = (float) (Math.random() * 6 - 3);
+        }
     }
 
-    Paint paint = new Paint();
-    int x = 0;
-    long lastTime = System.currentTimeMillis();
+
     @Override
     protected void onDraw(Canvas canvas){
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(x, 300, 40, paint);
-        // готовим x для следущего кадра
-        long nowTime = System.currentTimeMillis();
-        x += 2;
-        lastTime = nowTime;
+        // отрисовываем все шарики
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
+        //запрашиваем перерисовку
         invalidate();
     }
 }
